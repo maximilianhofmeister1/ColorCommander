33e351822aa29e22e8c0f9b59c653e253112e797
Maximilian <78014275+maximilianhofmeister1@users.noreply.github.com>
2024-12-24 15:36:37 +0100

    EventScanner and pipeFills started

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,47 @@
 import codedraw.*;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     static CodeDraw window = new CodeDraw(1000, 600);
     static double marginX = (int) (window.getWidth() * 0.2);
     static double marginY = (int) (window.getHeight() * 0.1);
+    static GameStats game = new GameStats();
 
 
     public static void main(String[] args) {
-        //game variables
-        GameStats game = new GameStats();
-
         buildGameInterface(game.speed, game.time, game.pickerLetters, game.pipeLetters);
+        char[] keysPressed = new char[2];
+
+        while (!window.isClosed()) {
+            for (var e : window.getEventScanner()) {
+                switch (e) {
+                    case KeyDownEvent a -> {
+                        if(keysPressed[0]=='\u0000'){
+                            keysPressed[0] = Character.toUpperCase(a.getChar());
+                        }
+                        else{
+                            keysPressed[1] = Character.toUpperCase(a.getChar());
+                            fillUp(keysPressed);
+                            keysPressed = new char[2];
+                        }
+                    }
+                    case MouseClickEvent a -> {
+                        checkWindowClick(a.getX(), a.getY());
+                    }
+                    default -> {
+                    }
+                }
+            }
+            buildGameInterface(game.speed, game.time, game.pickerLetters, game.pipeLetters);
+        }
+
+
     }
 
     private static void buildGameInterface(double currentSpeed, int time, char[] pickerLetters, char[] pipeLetters) {
+        window.clear();
         //Menu
         window.setTitle("Multitasking Test");
         window.drawText(10, 10, "Restart");
@@ -62,8 +88,42 @@ public class Main {
                     marginY + (2 * colWidth) + pipeHeight + 10,
                     "" + pipeLetters[i]
             );
+
+            //Pipe Fills
+            for (int fillCount = 0; fillCount < pickerLetters.length; fillCount++) {
+                double fillWidth = colWidth / pickerLetters.length;
+                Point2D fillsXYCoordinate = new Point2D(
+                        (colWidth * 1.5 * i) + marginX + (fillWidth * fillCount),
+                        marginY + (2* colWidth) + pipeHeight
+                );
+                window.fillRectangle(fillsXYCoordinate.getX(), fillsXYCoordinate.getY(), fillWidth, 50);
+            }
         }
 
-        window.show();
+        window.show(40);
+    }
+
+    private static void fillUp(char[] keysPressed){
+        //TODO: fillUp-Funktion
+        //System.out.println(Arrays.toString(keysPressed));
+    }
+
+    //selects correct function (restart() or settings())
+    private static void checkWindowClick(int x, int y) {
+        if (x < 100 && y < 40) {
+            restart();
+        } else if (x > window.getWidth() - 100 && y < 40) {
+            settings();
+        }
+    }
+
+    private static void restart() {
+        //TODO: Save Game-Stats as log to File
+        game = new GameStats();
+    }
+
+    private static void settings() {
+        System.out.println("Settings-Function");
+        //TODO: Settings-Function
     }
 }
