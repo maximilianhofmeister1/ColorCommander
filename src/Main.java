84b06fb06b31907b00d2e995eb0d974d951546d5
Maximilian <78014275+maximilianhofmeister1@users.noreply.github.com>
2024-12-24 13:28:45 +0100

    Methode buildGameInterface

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,70 @@ import codedraw.*;
 import java.awt.*;
 
 public class Main {
+    static CodeDraw window = new CodeDraw(1000, 600);
+    static double marginX = (int) (window.getWidth() * 0.2);
+    static double marginY = (int) (window.getHeight() * 0.1);
+
+
     public static void main(String[] args) {
-        CodeDraw window = new CodeDraw(1000, 600);
+        //game variables
+        int time = 0;
+        int speed = 1;
+        char[] pickerLetters = {'A', 'B', 'C', 'D'};
+        char[] pipeLetters = {'E', 'F', 'G', 'H'};
+
+        buildGameInterface(speed, time, pickerLetters, pipeLetters);
+    }
+
+    private static void buildGameInterface(double currentSpeed, int time, char[] pickerLetters, char[] pipeLetters) {
+        //Menu
         window.setTitle("Multitasking Test");
+        window.drawText(10, 10, "Restart");
+        window.drawText(300, 10, "current Speed: " + currentSpeed);
+        window.drawText(600, 10, "Time: " + (time / 60) + ":" + (((time % 60) < 10) ? "0" : "") + (time % 60));
+        window.drawText(window.getWidth() - 80, 10, "Settings");
+
+        //Game
+        double gameWidth = window.getWidth() - (2 * marginX);
+        double gameHeight = window.getHeight() - (2 * marginY);
+        double colWidth = gameWidth / 5.5;
+        Color[] colors = {Palette.RED, Palette.GREEN, Palette.BLUE, Palette.ORANGE};
+
+        //ColorPickers
+        for (int i = 0; i < 4; i++) {
+            window.setColor(colors[i]);
+            window.fillSquare(
+                    (colWidth * 1.5 * i) + marginX,
+                    marginY,
+                    colWidth
+            );
+
+            window.setColor(Palette.BLACK);
+            window.drawText(
+                    (colWidth * 1.5 * i) + marginX + (colWidth / 2 - 5),
+                    marginY + colWidth + 10,
+                    "" + pickerLetters[i]
+            );
+        }
+
+        //Pipes
+        double pipeHeight = (window.getHeight() - marginY) - (marginY + (2 * colWidth));
+        for (int i = 0; i < 4; i++) {
+            window.setColor(Palette.BLACK);
+            window.drawRectangle(
+                    (colWidth * 1.5 * i) + marginX,
+                    marginY + (2 * colWidth),
+                    colWidth,
+                    pipeHeight
+            );
+
+            window.drawText(
+                    (colWidth * 1.5 * i) + marginX + (colWidth / 2 - 5),
+                    marginY + (2 * colWidth) + pipeHeight + 10,
+                    "" + pipeLetters[i]
+            );
+        }
+
+        window.show();
     }
 }
