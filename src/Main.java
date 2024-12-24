ba08ae45a2631913921e6d91d0ff3c6e94088504
Maximilian <78014275+maximilianhofmeister1@users.noreply.github.com>
2024-12-24 11:41:28 +0100

    init project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import codedraw.*;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CodeDraw window = new CodeDraw(1000, 600);
+        window.setTitle("Multitasking Test");
+    }
+}
